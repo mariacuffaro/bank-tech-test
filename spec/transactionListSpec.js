9c804a9493687e4c6872ff915f56881e3fe102a2
Student <student@Makerss-MacBook-Pro-8.local>
2019-10-21 16:25:04 +0100

    added TransactionList

--- spec/transactionListSpec.js
+++ spec/transactionListSpec.js
@@ -1,6 +1,14 @@
-describe('Transactions', function() {
+describe('TransactionList', function() {
 
-
-it('Holds a list of transactions', function(){
+it('can be empty', function(){
+  let list = new TransactionList();
+  expect(list.transactions.length).toEqual(0)
   });
+it('can contain a transaction', function(){
+  let list = new TransactionList();
+  let transaction = new Transaction();
+  list.addTransaction(transaction);
+  expect(list.transactions.length).toEqual(1)
+  });
+
 });
--- src/transactionList.js
+++ src/transactionList.js
@@ -1,6 +1,11 @@
 (function(exports) {
 
   function TransactionList() {
+    this.transactions = [];
+  };
+
+  TransactionList.prototype.addTransaction = function(transaction) {
+    this.transactions.push(transaction);
   };
 
   exports.TransactionList = TransactionList
